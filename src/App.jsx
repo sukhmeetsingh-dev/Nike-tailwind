118155b4fbeb022d8a79ce5a8ba271cb5e00a3dd
Sukhmeet <sukhmeetsingh645@gmail.com>
2023-11-01 23:26:16 +0530

    components added & making left side

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,48 @@
+import CustomerReviews from './sections/CustomerReview';
+import Footer from './sections/Footer';
+import Hero from './sections/Hero';
+import PopularProducts from './sections/PopularProducts';
+import Services from './sections/Services';
+import SpecialOffer from './sections/SpecialOffer';
+import Subscribe from './sections/Subscribe';
+import SuperQuality from './sections/SuperQuality';
+import Nav from './components/Nav';
+
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav />
 
-  <section className="xl:padding-1 wide:padding-r padding-b">
-    Hero
-  </section>
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
 
-  <section className="padding">
-    PopularProducts
-  </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
 
-  <section className="padding">
-    SuperProducts
-  </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
 
-  <section className="padding-x py-10">
-    Services
-  </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
 
-  <section className="padding">
-    SpecialOffer
-  </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
 
-  <section className="bg-pale-blue padding">
-    CustomerReviews
-  </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
 
-  <section className="padding-x sm:py-32 py-16 w-full">
-    Subscribe
-  </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
 
-  <section className="bg-black padding-x padding-t pb-8">
-    Footer
-  </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
 
   </main>
 );
